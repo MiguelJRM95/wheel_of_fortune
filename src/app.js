52f6a68b7c8334a475d87fd12c13581ad6a227f7
MiguelJRM95 <mmiguelitoworkout@gmail.com>
2021-11-14 19:58:31 +0100

    active navbar link funcionality done

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+app.get('/win', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/win.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
