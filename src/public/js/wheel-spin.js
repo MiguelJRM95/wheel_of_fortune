c6ecd606dd97b5f1d7b2e3b461029317547aa0d1
MiguelJRM95 <mmiguelitoworkout@gmail.com>
2021-11-17 20:44:40 +0100

    get prize from wheel feature done

--- src/public/js/wheel-spin.js
+++ src/public/js/wheel-spin.js
@@ -1,8 +1,37 @@
 let container = document.querySelector(".wheel-container");
 let btn = document.getElementById("spin");
+let arrow = document.querySelector(".arrow");
 let number = Math.ceil(Math.random() * 1000);
+let elementClass = null;
+
+const getPrize = (elementClass) => {
+	switch (elementClass) {
+		case "one":
+		case "two":
+		case "three":
+		case "four":
+		case "five":
+		case "six":
+		case "seven":
+		case "eight":
+		case "wheel-text":
+			let element = document.querySelector(`.${elementClass}`);
+			return element.textContent;
+			break;
+		default:
+			return false;
+			break;
+	}
+}
+
 
 btn.onclick = function () {
 	container.style.transform = "rotate(" + number + "deg)";
 	number += Math.ceil(Math.random() * 1000);
 }
+
+container.addEventListener('transitionend', ()=>{
+	elementClass = document.elementFromPoint(arrow.getBoundingClientRect().x+30, arrow.getBoundingClientRect().y-20).className;
+	console.log(getPrize(elementClass));
+})
+
