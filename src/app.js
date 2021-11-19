3511eb1a897aef0fa3a9ffe322b71255fafe020c
MiguelJRM95 <mmiguelitoworkout@gmail.com>
2021-11-19 01:08:53 +0100

    post email funtion done, not test yet

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
+const emailHost = process.env.EMAIL_HOST || "smtp.mailtrap.io";
+const emailPort = process.env.EMAIL_PORT || 2525;
+const emailUser = process.env.EMAIL_USER || "a558b8d1e7d5cd";
+const emailPwd = process.env.EMAIL_PWD || "151995bd37d8f6";
+const emailAddrs = process.env.EMAIL_ADDRS || "miguel@mail.spain";
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const faunadb = require('faunadb');
 const q = faunadb.query;
 const adminClient = new faunadb.Client({ secret: process.env.FAUNADB_ADMIN_SECRET || 'fnAEYF9RMoACSYdCNvXRkitKbQJEyvkEUp8BnssU' });
+
+const transporter = nodemailer.createTransport({
+  host: emailHost,
+  port: emailPort,
+  secure: false, 
+  auth: {
+    user: emailUser, 
+    pass: emailPwd, 
+  },
+});
 
 
 app.use(express.static(path.join(__dirname,'public')));
@@ -30,11 +50,30 @@ app.get('/quotes', async (req, res) => {
         q.Paginate(q.Documents(q.Collection("quotes"))),
         q.Lambda("X", q.Get(q.Var("X")))
       )
-    )
+    );
 
-    res.status(200).json(quotes)
+    res.status(200).json(quotes);
   } catch (error) {
-    res.status(500).json({error: error.description})
+    res.status(500).json({error: error.description});
+  }
+});
+
+app.post('/email', async (req, res) => {
+  try{
+    let nameSender = req.body.name;
+    let sender = req.body.email;
+    let emailBody = req.body.emailBodyContent;
+    let subject = req.body.sub;
+
+    let info = await transporter.sendMail({
+      from: `"${nameSender}" <${sender}>`, 
+      to: `${emailAddrs}`, 
+      subject: `${subject}`, 
+      html: `<div>${emailBody}</div>`
+    });
+
+  }catch{
+    console.error;
   }
 });
 
