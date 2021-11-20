70989c3b470b1bce1cf546fd0455e5402131210b
MiguelJRM95 <mmiguelitoworkout@gmail.com>
2021-11-20 17:04:07 +0100

    buttons_nav.js comments added

--- src/public/js/buttons_nav.js
+++ src/public/js/buttons_nav.js
@@ -1,13 +1,37 @@
+/** 
+*    Functionality for color the navbar link active for the current page
+*    where the user is at.
+*/
+
+
+/**
+ * each link is a page serve 
+ * the home page is the quote link
+ * Because every time the user go there
+ * a new quote is displayed
+ */
+
 let homePageLink = document.getElementById("quote");
 let winPageLink = document.getElementById("win");
 let aboutPageLink = document.getElementById("about");
 let contactPageLink = document.getElementById("contact");
 
+/**
+ *  In case the app grows and a new level in the route would added
+ *  This function will need to be modified
+ * @returns the location the user is
+ */
 const currentPage = () => {
     let location = window.location.href;
     return location.split('/')[3];
 }
 
+/**
+ * This functions recieve the current page location of the above function currentPage()
+ * the add a class oc active to color the background of the link
+ * don't return anything
+ * @param {string} page 
+ */
 const activePage = (page) => {
     switch (page) {
         case "quote":
@@ -31,6 +55,10 @@ const activePage = (page) => {
     }
 }
 
+/**
+ * Each time the window load it will know where the user is at and color 
+ * highlight the link active
+ */
 window.addEventListener('load', () => {
     activePage(currentPage());
 })
