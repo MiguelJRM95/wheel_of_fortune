bcb005c8d4f86c05cc8f1d05d05cea2f74807a9f
MiguelJRM95 <mmiguelitoworkout@gmail.com>
2021-11-20 21:47:50 +0100

    table_controls.js comments completed

--- src/public/js/table_control.js
+++ src/public/js/table_control.js
@@ -1,5 +1,13 @@
+/**
+ * @var clearTable button the user press in order to keep spinning the wheel
+ */
 let clearTable = document.getElementById("clear-prizes");
 
+/**
+ * 
+ * @param {String} winner the Json object saved in the local storage
+ * @param {int} iterator the number of winners saved in the  local storage
+ */
 let printWinner = (winner, iterator) =>{
     let row = document.createElement("tr");
     const luckyW = winner["luckyWinner"]==false?`\u274C`:`\u{1F4B0}`;
@@ -10,6 +18,10 @@ let printWinner = (winner, iterator) =>{
     tableBody.appendChild(row);
 }
 
+/**
+ * Each time the window load in the win.html
+ * the local storage will be check in order to find if the user have ever spin the wheel
+ */
 window.addEventListener('load', ()=>{
     for (let i = 0; i < localStorage.length; i++) {
         let winner = JSON.parse(localStorage.getItem(`winner-${i+1}`));
@@ -17,6 +29,10 @@ window.addEventListener('load', ()=>{
     }
 })
 
+/**
+ * When the user click on this button
+ * the local storage is emptied and the table cleared
+ */
 clearTable.onclick = ()=>{
     localStorage.clear();
     winnerCounter = 0;
