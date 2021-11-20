63552cc2cd9e29a3241f2be6176687933861aa42
MiguelJRM95 <mmiguelitoworkout@gmail.com>
2021-11-20 14:07:18 +0100

    some styles of about people cards modified

--- src/public/js/wheel-pop-up.js
+++ src/public/js/wheel-pop-up.js
@@ -1,16 +1,36 @@
+const textToShow = () => {
+    let route = window.location.pathname;
+    console.log(route);
+    switch (route) {
+        case '/win':
+            return `Hi, Visitor! This is our Wheel Of Fortune, you have three free spins,
+                    but remember, you first need to select a prize of your choise, Good Luck!
+                    To play you only need to prees the spin button in the middle of the wheel,
+                    the more times you press, the more speed you give the wheel.
+            
+                                                👇`;
+        case '/':
+            return `Hello World`;
+        default:
+            return route;
+            break;
+    }
+}
+
+
 let backgroundPopUp = document.createElement("div");
 backgroundPopUp.style.width = "100vw";
 backgroundPopUp.style.height = "100vh";
-backgroundPopUp.style.backgroundColor= "#2e2e2e9c";
-backgroundPopUp.style.position= "absolute";
-backgroundPopUp.style.top= "0";
-backgroundPopUp.style.zIndex= "99";
+backgroundPopUp.style.backgroundColor = "#2e2e2e9c";
+backgroundPopUp.style.position = "absolute";
+backgroundPopUp.style.top = "0";
+backgroundPopUp.style.zIndex = "99";
 
 
 let innerDivContent = document.createElement("div");
 innerDivContent.style.width = "70vw";
 innerDivContent.style.height = "50vh";
-innerDivContent.style.backgroundColor= "#fff";
+innerDivContent.style.backgroundColor = "#fff";
 innerDivContent.style.margin = "12% auto";
 innerDivContent.style.borderRadius = "25px";
 innerDivContent.style.display = "flex";
@@ -30,22 +50,17 @@ let winExplanation = document.createElement("p");
 winExplanation.style.textAlign = "center";
 winExplanation.style.width = "70%";
 winExplanation.style.margin = "0 auto";
-winExplanation.innerText = `Hi, Visitor! This is our Wheel Of Fortune, you have three free spins,
-                            but remember, you first need to select a prize of your choise, Good Luck!
-                            To play you only need to prees the spin button in the middle of the wheel,
-                            the more times you press, the more speed you give the wheel.
-                            
-                                                                👇`;
+winExplanation.innerText = textToShow();
 
 
-window.addEventListener('load', () =>{
-    document.body.appendChild(backgroundPopUp);
-    backgroundPopUp.appendChild(innerDivContent);
-    innerDivContent.appendChild(winExplanation);
-    innerDivContent.appendChild(button);
-})
+    window.addEventListener('load', () => {
+        document.body.appendChild(backgroundPopUp);
+        backgroundPopUp.appendChild(innerDivContent);
+        innerDivContent.appendChild(winExplanation);
+        innerDivContent.appendChild(button);
+    })
 
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
     backgroundPopUp.remove();
 })
